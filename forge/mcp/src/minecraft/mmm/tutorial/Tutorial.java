eb5347a5a520e4c248ac4b1090efaa0bbd34f971
bekiral <kiralbaris@gmail.com>
2013-05-07 23:16:28 +0300

    added 1st class

--- forge/mcp/src/minecraft/mmm/tutorial/Tutorial.java
+++ forge/mcp/src/minecraft/mmm/tutorial/Tutorial.java
@@ -1,5 +1,19 @@
 package mmm.tutorial;
 
-public class Tutorial {
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
 
+@Mod(modid = Tutorial.modid, name = "MMM Mod", version = "1.5.2")
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
+public class Tutorial
+{
+       public static final String modid = "MMM_MMMMod";
+      
+       @Init
+       public void load(FMLInitializationEvent event)
+       {
+        
+       }
 }
