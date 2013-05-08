308191b2793749ca04583b6b94efcaeef0e8315d
Indicado <cyysio@wp.pl>
2013-05-08 17:42:41 +0200

    .

--- forge/mcp/src/minecraft/mmm/esm/entity/Entity.java
+++ forge/mcp/src/minecraft/mmm/esm/entity/Entity.java
@@ -4,16 +4,18 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.EntityList;
+import net.minecraft.client.Minecraft;
 
 @Mod(modid = Entity.modid, name = "esm", version = "1.5.2")
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 
 
-public class Entity
+public class Entity 
 {
        public static final String modid = "Entity";
        
@@ -22,6 +24,6 @@ public class Entity
        public void load(FMLInitializationEvent event)
        {
     	 EntityList.addMapping(Zombie.class,"Zombie",54, 44975, 7969893);
-    	 
+    	 Minecraft.loadWorld(Player.class)
        }
 }
