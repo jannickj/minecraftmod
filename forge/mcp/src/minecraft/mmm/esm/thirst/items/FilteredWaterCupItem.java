1613ba9cc9768ca262bc89db83f726033dedd0d6
bekiral <kiralbaris@gmail.com>
2013-05-10 01:06:01 +0300

    added FilteredWaterCupItem.java

---
+++ forge/mcp/src/minecraft/mmm/esm/thirst/items/FilteredWaterCupItem.java
@@ -0,0 +1,32 @@
+package mmm.esm.thirst.items;
+
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import mmm.esm.thirst.ItemDrink;
+import mmm.esm.thirst.ThirstLoader;
+
+public class FilteredWaterCupItem extends ItemDrink {
+
+	public FilteredWaterCupItem(int id) {
+		super(id, 2, 50);
+		setMaxStackSize(1);
+		setCreativeTab(CreativeTabs.tabMisc);
+		setUnlocalizedName("filteredWaterCup");
+	}
+	
+	
+	public void registerIcons(IconRegister iconRegister)
+	{
+		
+	         this.itemIcon = iconRegister.registerIcon("ThirstMod:cupWater_filtered");
+	}
+	
+	public ItemStack onEaten(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
+    {
+		super.onEaten(par1ItemStack, par2World, par3EntityPlayer);
+		return new ItemStack(ThirstLoader.woodenCup);
+    }
+}
